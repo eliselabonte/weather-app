dfb46e69a42105fce790adc703df840f051e38c1
Elise_LaBonte <emlabonte714@gmail.com>
2021-07-27 11:50:47 -0400

    created function to get lat and long from geocity call

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,55 @@
-$(function()    {
+$(document).ready(function () {
+    const $searchByCity = $('#searchByCity');
+    const $searchSubmitButton = $('#searchSubmit');
+    const $pastCityList = $('#pastCityList');
+    const $todayDate = $('#todayDate');
+    const $cityName = $('#cityName');
+    const $weatherDescription = $('#weatherDescription');
+    const $temperature = $('#temperature');
+    const $humidity = $('#humidity');
+    const $windspeed = $('#windspeed');
+    const $uvIndex = $('#uvIndex');
+    const $dayOneWeather = $('#dayOne');
+    const $dayTwoWeather = $('#dayTwo');
+    const $dayThreeWeather = $('#dayThree');
+    const $dayFourWeather = $('#dayFour');
+    const $dayFiveWeather = $('#dayFive');
     
-})
+    const apiKey = "d2a2cad4f1d38ef0dfacee769aba90c6";
+    
+    console.log($searchByCity)
+;            // populate page with weather data on submit
+    $searchSubmitButton.click(function()  {
+            const city = $searchByCity.val();
+            console.log(city);
+            getCity(city);
+        })    
+    
+        // need a function here to display past searches
+    
+        function displayPastCities()    {
+            const city = $('p');
+    
+            
+            $pastCityList.append(city);
+        }
+        // utilize local storage
+        // on load, display data from Boston or something
+        // create elements to populate 5-day forecast items
+
+        function getCity(city)  {
+
+            fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${apiKey}`)
+            .then(function(response)    {
+                return response.json();
+            })
+            .then(function(data)    {
+                console.log(data);
+            })
+        }
+
+});
+
+
+
+
